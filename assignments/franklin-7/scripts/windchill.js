1c49b279d149667905c0d3cae91c40ca7a59d9d6
starr06 <35233209+starr06@users.noreply.github.com>
2018-02-20 20:20:07 -0600

    up

--- assignments/franklin-7/scripts/windchill.js
+++ assignments/franklin-7/scripts/windchill.js
@@ -1,17 +1,14 @@
-//function doInputOutput() {
-//	var t = parseFloat(document.getElementById('Temperature').value);
-//	var s = parseFloat(document.getElementById('Wind Speed').value);
-
-var t = (90 + 66) / 2;
-var s = 5;
-
-var ans = windChill(t, s);
-	document.getElementById('feelsLike').innerHTML = ans;
-	//document.getElementById('outputDiv').innerHTML = "With wind chill the" +
-	//" temperature is " + ans.toFixed(00) + " degrees Farhrenheit.";
+var h = parseFloat(document.getElementById('high').textContent);
+var l = parseFloat(document.getElementById('low').textContent);
+var s = parseFloat(document.getElementById('wind').textContent);
+var t = (h + l) / 2;
 
 function windChill(tempF, speed) {
 	var f = 35.74 + .6215 * tempF - 35.75 * Math.pow(speed, .16) +
 	.4275 * tempF * Math.pow(speed, .16);
-	return f.toFixed(00);
+	return Math.round(f);
 }
+
+var ans = windChill(t, s);
+	document.getElementById('feelsLike').innerHTML = ans;
+	
