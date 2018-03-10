dfae95b5841e9c82dcfe8d65c68e8222555c0386
starr06 <35233209+starr06@users.noreply.github.com>
2018-03-10 16:45:51 -0600

    challenge

--- assignments/lesson-9/scripts/wu-weather.js
+++ assignments/lesson-9/scripts/wu-weather.js
@@ -5,13 +5,27 @@ weatherObject.open('GET', 'http://api.wunderground.com/api/cb37f191fa75ceaa/cond
 weatherObject.send();
 
 weatherObject.onload = function () {
- 
     var weatherInfo = JSON.parse(weatherObject.responseText);
-    console.log(weatherInfo);
+    //console.log(weatherInfo);
     
     document.getElementById('currentWeather').innerHTML = weatherInfo.current_observation.weather;
     document.getElementById('currentTemp').innerHTML = weatherInfo.current_observation.temp_f;
     document.getElementById('wind').innerHTML = weatherInfo.current_observation.wind_mph;
-    
     document.getElementById('currentIcon').src = weatherInfo.current_observation.icon_url;
+    document.getElementById('feelsLike').innerHTML = weatherInfo.current_observation.feelslike_f
+    
+}
+
+var weatherForecast = new XMLHttpRequest();
+
+weatherForecast.open('GET', 'http://api.wunderground.com/api/cb37f191fa75ceaa/forecast/q/MN/Franklin.json', true );
+
+weatherForecast.send();
+
+weatherForecast.onload = function () {
+    var weatherInfo = JSON.parse(weatherForecast.responseText);
+    //console.log(weatherInfo);
+
+    document.getElementById('weatherDesc').innerHTML =weatherInfo.forecast.txt_forecast.forecastday["0"].fcttext;
+
 }
